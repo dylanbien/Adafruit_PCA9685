e0e0c51a76b8184495e941f7d5d55a3edc916237
ngetachew <natugetachew@gmail.com>
2019-04-04 10:03:21 -0700

    Documentation/ cleaned up

--- Adafruit_Ease_Lib.py
+++ Adafruit_Ease_Lib.py
@@ -1,3 +1,4 @@
+#made by mangochildren
 import Adafruit_PCA9685
 
 
@@ -10,7 +11,11 @@ class Adafruit_Ease_Lib():
     SERVO_MID_TIME = 336
     SERVO_MAX_TIME = 560
 
-    def __init__(self,offsets = None, **kwargs):
+    '''
+    offests - if the servo limits are known to be different from the standards, provide the 
+    new servo limits in this array. Defaults to noe
+    '''
+    def __init__(self,offsets = None):
         self.adafruit = Adafruit_PCA9685.PCA9685()
         self.num_pins = 16
         if not (offsets  == None):
@@ -18,11 +23,14 @@ class Adafruit_Ease_Lib():
         print('Adafruit initialized')
 
 
-
-
     def change_frequency(self, freq):
         self.adafruit.set_pwm_freq(freq)
 
+    '''
+    left -  period required for the servo to be at the minimum position/ 0 degrees
+    center - period required to move to the center position/ 90 degrees
+    right - period required to move to the maximum position/ 180 degrees
+    '''
     def set_servo_limits(self, left, center, right):
         Adafruit_Ease_Lib.SERVO_LOW_TIME = left
         Adafruit_Ease_Lib.SERVO_MID_TIME = center
@@ -37,6 +45,13 @@ class Adafruit_Ease_Lib():
     def convert_period_to_freq(self, period):
         return 1/period
 
+    '''
+    FOR ALL THE FOLLOWING FUNCTIONS:
+    
+    *If pin is an array, the function will loop through the array of pins and perform the given task on all of them
+    *If pin is a string that says 'all', the function will loop through all the pins and perform the given task on them
+    *If pin is an int, the function will perform the given task on that specific pin.
+    '''
     def set_high(self, pin):
         if pin == 'all':
             self.adafruit.set_all_pwm(HIGH, 0)
@@ -55,6 +70,10 @@ class Adafruit_Ease_Lib():
         else:
             self.adafruit.set_pwm(pin, LOW, 0)
 
+    '''
+    percentage - On time percentage in the duty cycle. Use the change_percentage_servo() 
+    function for servo movement
+    '''
     def change_percentage(self, pin, percent):
         if percent == 100:
             self.set_high(pin)
@@ -73,28 +92,67 @@ class Adafruit_Ease_Lib():
         else:
             self.adafruit.set_pwm(pin, percentage, LOW)
 
+    '''
+    percent - percent of a 180 degree rotation the servo is meant to turn to
+    '''
     def change_percentage_servo(self, pin, percent):
         self.adafruit.set_pwm_freq(50)
         if percent == 100:
-            #self.adafruit.set_pwm(pin,380,360)
-            self.adafruit.set_pwm(pin, 0, Adafruit_Ease_Lib.SERVO_MAX_TIME)
+
+            if pin == 'all':
+                self.adafruit.set_all_pwm(0, Adafruit_Ease_Lib.SERVO_MAX_TIME)
+            elif isinstance(pin, list):
+                for i in pin:
+                    self.adafruit.set_pwm(i,0, Adafruit_Ease_Lib.SERVO_MAX_TIME)
+            else:
+                self.adafruit.set_pwm(pin, 0, Adafruit_Ease_Lib.SERVO_MAX_TIME)
             print("moving to right")
 
         elif percent == 0:
-            self.adafruit.set_pwm(pin, 0, Adafruit_Ease_Lib.SERVO_LOW_TIME)
+
+            if pin == 'all':
+                self.adafruit.set_all_pwm(0, Adafruit_Ease_Lib.SERVO_LOW_TIME)
+            elif isinstance(pin, list):
+                for i in pin:
+                    self.adafruit.set_pwm(i, 0, Adafruit_Ease_Lib.SERVO_LOW_TIME)
+            else:
+                self.adafruit.set_pwm(pin, 0, Adafruit_Ease_Lib.SERVO_LOW_TIME)
+
             print("moving to center")
+
         elif percent == 50:
-            print(Adafruit_Ease_Lib.SERVO_MID_TIME)
-            self.adafruit.set_pwm(pin, 0, Adafruit_Ease_Lib.SERVO_MID_TIME)
-            return
+
+            if pin == 'all':
+                self.adafruit.set_all_pwm(0, Adafruit_Ease_Lib.SERVO_MID_TIME)
+            elif isinstance(pin, list):
+                for i in pin:
+                    self.adafruit.set_pwm(i,0, Adafruit_Ease_Lib.SERVO_MID_TIME)
+            else:
+                self.adafruit.set_pwm(pin, 0, Adafruit_Ease_Lib.SERVO_MID_TIME)
+
         elif percent < 50:
+
             percentage = percent
             value = int(percentage * (1/50) * (Adafruit_Ease_Lib.SERVO_MID_TIME-Adafruit_Ease_Lib.SERVO_LOW_TIME) + Adafruit_Ease_Lib.SERVO_LOW_TIME)
             print('moving to ' + str(value))
-            self.adafruit.set_pwm(pin, 0, value)
+            if pin == 'all':
+                self.adafruit.set_all_pwm(0, value)
+            elif isinstance(pin, list):
+                for i in pin:
+                    self.adafruit.set_pwm(i, 0, value)
+            else:
+                self.adafruit.set_pwm(pin, 0, value)
+
         elif percent > 50:
+
             percentage = (percent - 50)
             value = int(percentage * (1 / 50) * (Adafruit_Ease_Lib.SERVO_MAX_TIME - Adafruit_Ease_Lib.SERVO_MID_TIME) + Adafruit_Ease_Lib.SERVO_MID_TIME)
             print('moving to ' + str(value))
-            self.adafruit.set_pwm(pin, 0, value)
 
+            if pin == 'all':
+                self.adafruit.set_all_pwm(0, value)
+            elif isinstance(pin, list):
+                for i in pin:
+                    self.adafruit.set_pwm(i, 0, value)
+            else:
+                self.adafruit.set_pwm(pin, 0, value)
